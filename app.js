698307ea13988c58be60d51389cc784c64b0918b
Liso <lisonyandu@gmail.com>
2021-03-19 16:40:33 +0200

    Final commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/views/index.html"));
+  //__dirname : It will resolve to your project folder.
+});
+
+router.get("/about", function (req, res) {
+  res.sendFile(path.join(__dirname + "/views/about.html"));
+});
+
+//add the router
+app.use("/", router);
+app.listen(process.env.port || 3000);
+
+console.log("Running at Port 3000");
